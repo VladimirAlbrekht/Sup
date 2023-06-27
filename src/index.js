4e110ec39ce940b9e9422c8dbb7a178e8e021897
Vladimir Albrekht <vladimir_albreht@mail.ru>
2023-06-27 13:09:14 +0800

    Start package

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles/index.css'; 
+
+console.log('Helldddo!') 
+
+
+
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers); // 4, 6, 10 
